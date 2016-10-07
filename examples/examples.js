5722f56425379c75979cf14393f5270105437639
Christopher Barry <me@chrisb.xyz>
2016-10-07 11:54:19 -0400

    Added promises

--- examples/examples.js
+++ examples/examples.js
@@ -10,7 +10,9 @@ news.getSources({
 	language: 'en',
 	country: 'us',
 	callback(res) {
-		console.log(res);
+		if (res.success) {
+			console.log(res);
+		}
 	}
 });
 
@@ -18,6 +20,30 @@ news.getArticles({
 	source: 'ars-technica',
 	sortBy: 'latest',
 	callback(res) {
-		console.log(res);
+		if (res.success) {
+			console.log(res);
+		} else {
+			console.log(res);
+		}
 	}
+});
+
+//You can also use promises!
+news.getSources({
+	category: 'technology',
+	language: 'en',
+	country: 'us'
+}).then(function(res) {
+	console.log(res);
+}).catch(function(err) {
+	console.log(err);
+});
+
+news.getArticles({
+	source: 'ars-technica',
+	sortBy: 'latest'
+}).then(function(res) {
+	console.log(res);
+}).catch(function(err) {
+	console.log(err);
 });
